3d29b8216953450e7eeaa6f52ccd8462fd8f5024
psos <ya.rpg2001@yandex.ru>
2024-06-02 15:54:01 +0500

    Add Input Masks

--- WebApplication1/Models/Entity/Patient.cs
+++ WebApplication1/Models/Entity/Patient.cs
@@ -10,13 +10,16 @@ namespace WebApplication1.Models.Entity
         [Required]
         [Display(Name = "Фамилия")]
         [MaxLength(50, ErrorMessage = "Превышена допустима длина строки")]
+        [RegularExpression(@"^[A-Za-zА-Яа-яЁё]+$", ErrorMessage = "Имя может содержать только буквы")]
         public string SName { get; set; } = string.Empty;
         [Required]
         [Display(Name = "Имя")]
         [MaxLength(50, ErrorMessage = "Превышена допустима длина строки")]
+        [RegularExpression(@"^[A-Za-zА-Яа-яЁё]+$", ErrorMessage = "Имя может содержать только буквы")]
         public string FName { get; set; } = string.Empty;
         [Display(Name = "Отчество")]
         [MaxLength(50, ErrorMessage = "Превышена допустима длина строки")]
+        [RegularExpression(@"^[A-Za-zА-Яа-яЁё]+$", ErrorMessage = "Имя может содержать только буквы")]
         public string MName { get; set; } = string.Empty;
         [Required]
         [Display(Name = "День рождения")]
@@ -24,6 +27,7 @@ namespace WebApplication1.Models.Entity
         [Required]
         [Display(Name = "Телефон")]
         [MaxLength(15, ErrorMessage = "Превышена допустима длина строки")]
+        [RegularExpression(@"^8 \d{3} \d{3} \d{2} \d{2}$", ErrorMessage = "Формат телефона должен быть 8 000 000 00 00")]
         public string Telephone { get; set; } = string.Empty;
     }
 }
